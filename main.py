42269361209fa5c430d90e8846613f24c62dbf4d
u1215ysk <u1215ysk@gmail.com>
2025-09-08 17:04:46 +0900

    最初のコミット

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from flask import Flask, request, abort
+from line_bot_sdk import LineBotApi, WebhookHandler
+from line_bot_sdk.exceptions import InvalidSignatureError
+from line_bot_sdk.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+# 環境変数から設定を取得
+channel_access_token = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN', None)
+channel_secret = os.environ.get('LINE_CHANNEL_SECRET', None)
+
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+# ヘルスチェック用のルート
+@app.route("/", methods=['GET'])
+def health_check():
+    return 'OK', 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
